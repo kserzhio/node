f74a93fe34f5b780954b83f7a420dbe07c89705a
kserzhio <kserzhio@gmail.com>
2019-06-19 20:18:54 +0300

    node app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import Koa from 'koa';
-import {port} from './utils/config'
-const app = new Koa();
-app.use(async ctx=>{
-    ctx.body = 'Hello Word'
-});
-app.listen(3000,() => console.log(`Server started port 3000 ${port}`));
+import app from './app'
+
+app.listen(3001,() => console.log(`Server started port 3001`));
